55965e68b4a43283ea5753b882c7d2cf816861e4
ChoiWonBeen <zad1264@naver.com>
2021-12-23 08:20:15 +0900

    add video autoplay
    * LazyLoading을 위한 저장 공간 변경 필요

--- src/components/Home/HomeComponent.tsx
+++ src/components/Home/HomeComponent.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
-
+import React, { useRef, useEffect, useLayoutEffect } from 'react';
+import Video from "../../assets/cute_bird.mp4";
+import "./HomeComponent.scss"
 const HomeComponent: React.FC = () => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  
+  useLayoutEffect(() => {
+    if(!videoRef.current) return;
+    videoRef.current.src = Video;
+    // videoRef.current.addEventListener('canplay', (e) => {
+    //   if(!videoRef.current) return;
+    //   videoRef.current.play();
+    // })
+  }, [])
+
   return (
-    <div>
-      홈페이지를 구성하는 컴포넌트
+    <div className='home-wrapper'>
+      <video ref={videoRef} height={"100%"} width={"100%"} muted autoPlay loop></video>
     </div>
   );
 }
