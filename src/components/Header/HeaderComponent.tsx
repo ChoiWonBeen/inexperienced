e0e605ad632d7ae2dec0115ed3d35af6836a99ed
ChoiWonBeen <zad1264@naver.com>
2021-12-27 20:00:30 +0900

    add fonts

--- src/components/Header/HeaderComponent.tsx
+++ src/components/Header/HeaderComponent.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./HeaderComponent.scss"
-import { ReactComponent as SettingIcon } from "../../assets/setting.svg";
+import { ReactComponent as SettingIcon } from "../../assets/svg/setting.svg";
 
 interface HeaderComponentProps {
   
@@ -9,6 +9,7 @@ interface HeaderComponentProps {
 const HeaderComponent: React.FunctionComponent<HeaderComponentProps> = () => {
   return ( 
     <header>
+      <h1>제목 부분입니다.</h1>
       <SettingIcon className='icon'/>
     </header>
    );
