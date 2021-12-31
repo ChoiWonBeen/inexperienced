14bacbf55a9ae615df6f7771c0ce5032f387e98e
WonBeen Choi <zad1264@naver.com>
2021-12-31 15:17:57 +0000

    add menu options

--- src/components/Main/MainMenu/MainMenuComponent.tsx
+++ src/components/Main/MainMenu/MainMenuComponent.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import "./MainMenuComponent.scss";
 
 export interface MainMenuProps {};
@@ -7,7 +6,9 @@ export interface MainMenuProps {};
 const MainMenu: React.FC<MainMenuProps> = (props) => {
   return (
     <div className="main-menu">
-      선택1
+      {[...Array(8)].map((n, idx) => {
+        return <div className="main-menu__option">{`선택${idx + 1}`}</div>
+      })}
     </div>
   );
 }
