fe05f5189cee9fa1df6988025984a09620034b6f
WonBeen Choi <zad1264@naver.com>
2022-01-01 14:26:27 +0000

    install framer-motion & add sample code

--- src/components/Main/MainMenu/MainMenuComponent.tsx
+++ src/components/Main/MainMenu/MainMenuComponent.tsx
@@ -1,13 +1,31 @@
 import React from "react";
+import { motion } from "framer-motion"
 import "./MainMenuComponent.scss";
 
 export interface MainMenuProps {};
 
 const MainMenu: React.FC<MainMenuProps> = (props) => {
+
+
   return (
     <div className="main-menu">
       {[...Array(8)].map((n, idx) => {
-        return <div className="main-menu__option">{`선택${idx + 1}`}</div>
+        return (
+          <motion.div
+            drag
+            dragConstraints={{
+              top: -50,
+              left: -50,
+              right: 50,
+              bottom: 50,
+            }}
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            className="main-menu__option"
+            key={idx}>
+            {idx + 1}
+          </motion.div>
+        )
       })}
     </div>
   );
