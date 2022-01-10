30bf7a948b6e40c2e2782d20934fe47ce811f9c8
WonBeen Choi <zad1264@naver.com>
2022-01-10 06:52:33 +0000

    add Title Head

---
+++ components/SEO.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
+
+export default function SEO(){
+  const router = useRouter();
+  let titleText = "HOME";
+
+  return (
+    <Head>
+      <title>
+        {`To Do It! | ${titleText}`}
+      </title>
+    </Head>
+  )
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
 import type { AppProps } from 'next/app'
 import { RecoilRoot } from 'recoil'
+import SEO from '../components/SEO'
+import Header from '../components/Header'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+
   return (
     <RecoilRoot>
+      <SEO/>
+      <Header/>
       <Component {...pageProps} />
     </RecoilRoot>
   )
