deec75cf1f8aa1e7c60c8b11a292b16281ea1e09
WonBeen Choi <zad1264@naver.com>
2022-01-15 14:29:50 +0000

    move container component to page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,26 @@
 import type { NextPage } from 'next';
-import Home from '../components/indexPage/Home';
+import styled from "styled-components";
+import Winrate from '../components/indexPage/Winrate';
+
+const HomeWrapper = styled.div`
+  height: 100%;
+  padding: 0 32px;
+  display: grid;
+  min-height: calc(100vh - 102px);
+`
+
+const WinrateGrid = styled.div`
+  display: grid;
+  grid-column: 2/2;
+  grid-row: 2/2;
+`
 
 export default function MainPage<NextPage>(){
-  return <Home/>
+  return (
+    <HomeWrapper>
+      <WinrateGrid>
+        <Winrate></Winrate>
+      </WinrateGrid>
+    </HomeWrapper>
+  )
 }
